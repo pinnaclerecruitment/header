fb80a6b9a8884b6589e5468cab41aed89465e8e0
pinnaclerecruitment <eddie@teampinnacle.com>
2025-05-19 15:24:46 -0700

    Update server.js
    Signed-off-by: pinnaclerecruitment <eddie@teampinnacle.com>

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
 
 app.get('/loxo-data', async (req, res) => {
     try {
-        const response = await fetch('https://app.loxo.co/api/pinnacle-recruitment-services', {
+        const response = await fetch('https://app.loxo.co/api/pinnacle-recruitment-services/job_categories', {
             method: 'GET',
             headers: {
                 'Authorization': `Bearer ${process.env.LOXO_TOKEN}`,
